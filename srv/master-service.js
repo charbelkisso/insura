5326fc97d90baa3b693298b77242c91232e70477
Kisso <charbel.kisso@cgi.com>
2020-08-16 18:14:30 +0200

    personal number check

--- srv/master-service.js
+++ srv/master-service.js
@@ -1,9 +1,17 @@
+const axios = require('axios');
+
 module.exports = (srv) => {
     srv.before('CREATE', 'PartnerIDs', (req) => {
         console.log(req);
     });
 
-    srv.before('CREATE', 'Partners', (req)=>{
-        console.log(req.data.idNumber.idNumber);
+    srv.before('CREATE', 'Partners', async (req) => {
+
+        const baseURL = "https://pnr-check-terrific-quoll-le.cfapps.eu10.hana.ondemand.com/pnrcheck/";
+
+        var reqUrl = baseURL + req.data.idNumber.idNumber;
+        var response = await axios.get(reqUrl);
+        if (response.data.status === 'Invalid')
+            req.reject('Invalid Personl Number');
     })
 }
